5cd701efea3ec31b1765beca94271a6f9dec285b
Naoto Inoue <inoue_naoto@cyberagent.co.jp>
2023-03-08 09:11:00 +0000

    initial commit for webpage

--- src/trainer/trainer/datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-from .publaynet import PubLayNetDataset
-from .rico import Rico25Dataset
-
-_DATASETS = [
-    Rico25Dataset,
-    PubLayNetDataset,
-]
-DATASETS = {d.name: d for d in _DATASETS}
--- src/trainer/trainer/global_configs.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-ROOT = f"{str(Path(__file__).parent)}/../../../download"
-KMEANS_WEIGHT_ROOT = f"{ROOT}/clustering_weights"
-DATASET_DIR = f"{ROOT}/datasets"
-FID_WEIGHT_DIR = f"{ROOT}/fid_weights/FIDNetV3"
-JOB_DIR = f"{ROOT}/pretrained_weights"
